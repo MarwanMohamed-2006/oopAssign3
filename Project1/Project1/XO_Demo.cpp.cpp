5c228c6fc027f403dd5c0056c6b4075f8e424801
Marwan Mohamed <marwanawad.7597@gmail.com>
2025-11-25 12:31:30 +0200

    Adding files

---
+++ Project1/Project1/XO_Demo.cpp.cpp
@@ -0,0 +1,136 @@
+﻿#include <iostream>
+#include <ctime>
+#include "BoardGame_Classes.h"
+#include "XO_Classes.h"
+#include "Numerical_TicTacToe_Classes.h"
+#include "Tic_Tac_Toe.h"
+using namespace std;
+
+int main() {
+    srand(static_cast<unsigned int>(time(0)));
+
+    int choice = 0;
+
+    do {
+        cout << "\n==============================\n";
+        cout << "🎮 FCAI Board Games Menu 🎮\n";
+        cout << "==============================\n";
+        cout << "1. Tic-Tac-Toe (X-O)\n";
+        cout << "2. SUS\n";
+        cout << "3. Four-in-a-Row\n";
+        cout << "4. 5x5 Tic-Tac-Toe\n";
+        cout << "5. Word Tic-Tac-Toe\n";
+        cout << "6. Misère Tic-Tac-Toe\n";
+        cout << "7. Diamond Tic-Tac-Toe\n";
+        cout << "8. 4x4 Tic-Tac-Toe\n";
+        cout << "9. Pyramid Tic-Tac-Toe\n";
+        cout << "10. Numerical Tic-Tac-Toe (Group)\n";
+        cout << "11. Obstacles Tic-Tac-Toe (Group)\n";
+        cout << "12. Infinity Tic-Tac-Toe (Group)\n";
+        cout << "13. Ultimate Tic-Tac-Toe (Bonus)\n";
+        cout << "14. Memory Tic-Tac-Toe (Bonus)\n";
+        cout << "15. Exit\n";
+        cout << "==============================\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+
+        case 1: {
+            UI<char>* game_ui = new XO_UI();
+            Board<char>* xo_board = new X_O_Board();
+            Player<char>** players = game_ui->setup_players();
+            GameManager<char> x_o_game(xo_board, players, game_ui);
+            x_o_game.run();
+
+            delete xo_board;
+            for (int i = 0; i < 2; ++i)
+                delete players[i];
+            delete[] players;
+            break;
+        }
+        case 2:
+            cout << "\n=== SUS Game ===\n";
+            break;
+
+        case 3:
+            cout << "\n=== Four-in-a-Row ===\n";
+            break;
+
+        case 4:
+        {
+            UI<char>* game_ui = new TicTacToe_UI();
+            Board<char>* tic5_board = new TicTacToe_Board();
+            Player<char>** players = game_ui->setup_players();
+            GameManager<char> tic5_game(tic5_board, players, game_ui);
+            tic5_game.run();
+
+            delete tic5_board;
+            for (int i = 0; i < 2; ++i)
+                delete players[i];
+            delete[] players;
+            break;
+        }
+        case 5:
+            cout << "\n=== Word Tic-Tac-Toe ===\n";
+            break;
+
+        case 6:
+            cout << "\n=== Misère Tic-Tac-Toe ===\n";
+            break;
+
+        case 7:
+            cout << "\n=== Diamond Tic-Tac-Toe ===\n";
+            break;
+
+        case 8:
+            cout << "\n=== 4x4 Tic-Tac-Toe ===\n";
+            break;
+
+        case 9:
+            cout << "\n=== Pyramid Tic-Tac-Toe ===\n";
+            break;
+
+        case 10:
+        {
+            UI<int>* game_ui = new Numerical_UI();
+            Board<int>* num_board = new Numerical_Board();
+            Player<int>** players = game_ui->setup_players();
+            GameManager<int> num_game(num_board, players, game_ui);
+            num_game.run();
+
+            delete num_board;
+            for (int i = 0; i < 2; ++i)
+                delete players[i];
+            delete[] players;
+            delete game_ui;
+            break;
+        }
+        case 11:
+            cout << "\n=== Obstacles Tic-Tac-Toe (Group) ===\n";
+            break;
+
+        case 12:
+            cout << "\n=== Infinity Tic-Tac-Toe (Group) ===\n";
+            break;
+
+        case 13:
+            cout << "\n=== Ultimate Tic-Tac-Toe (Bonus) ===\n";
+            break;
+
+        case 14:
+            cout << "\n=== Memory Tic-Tac-Toe (Bonus) ===\n";
+            break;
+
+        case 15:
+            cout << "GoodBye\n";
+            break;
+
+        default:
+            cout << "Invalid choice, try again!\n";
+            break;
+        }
+    } while (choice != 15);
+    return 0;
+}
