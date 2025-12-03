5f530781d2f1385b2570d33f07ffed8f9a0e482b
Marwan Mohamed <marwanawad.7597@gmail.com>
2025-12-03 14:49:08 +0200

    fizz

--- Misere_XO.h
+++ Misere_XO.h
@@ -8,15 +8,17 @@ private:
 	char blank_symbol = '.';
 public:
 	Misere_XO_Board();
-	bool update_board(Move<char>* move);
-	bool is_lose(Player<char>* player);
-	bool is_draw(Player<char>* player);
-	bool game_is_over(Player<char>* player);
-	bool three_in_a_row(char sym);
-	bool is_win(Player<char>* player);
+	bool update_board(Move<char>* move) override;
+	bool is_lose(Player<char>* player) override;
+	bool is_draw(Player<char>* player) override;
+	bool game_is_over(Player<char>* player) override;
+	bool is_win(Player<char>* player) override;
+	bool _3rows(char sym);
+	Move<char>* find_best_move(char computer_symbol, char human_symbol);
+	int minimax(bool is_maximizing, char sym1, char sym2);
 };
 class Misere_XO_UI : public UI<char> {
-public:
+	public:
 	Misere_XO_UI();
 	Player<char>* create_player(string& name, char symbol, PlayerType type);
 	virtual Move<char>* get_move(Player<char>* player);
